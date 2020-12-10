1beb4556fdaccf5c4cc077ddaf90512996235ac6
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-10 02:08:54 -0600

    commit tree 4af80fd14aed303c5d7184301b208ef1e06ec7f1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+require 'pry'
+
+class School
+
+    
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_reader :name
+    attr_reader :roster
+    
+    def add_student(name, grade)
+        if @roster.include?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+            #binding.pry
+        end
+    end
+
+end
+
+test_school = School.new("Test School")
+test_school.add_student("joe", 9)
+puts test_school.roster
+#binding.pry
+1
